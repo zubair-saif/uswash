8341259c2a0ea936401a2000a50f60d2cc33ccf7
zubairsaif700 <zubairsaif700@gmail.com>
2019-02-12 15:14:46 +0500

    df

---
+++ uwash/js/map.scripts.js
@@ -0,0 +1,21 @@
+
+google.maps.event.addDomListener(window, 'load', init);
+
+function init() {
+	var mapOptions = {
+		zoom: 14,
+		center: new google.maps.LatLng(40.672324, -74.357372), // New York
+		styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
+	};
+
+	var mapElement = document.getElementById('map');
+
+	var map = new google.maps.Map(mapElement, mapOptions);
+
+	var marker = new google.maps.Marker({
+		position: new google.maps.LatLng(40.672324, -74.357372),
+		map: map,
+		icon: 'http://unicoderbd.com/theme/html/uwash/images/location-pointer.png',
+		title: 'Snazzy!'
+	});
+}
